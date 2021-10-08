786ee8e32ee71c2a5e9d7587a1ade1d3f874e682
toast <smol@toast.cyou>
2021-10-08 12:08:37 -0400

    handled qr2term err type

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ use clipboard::{ClipboardProvider, ClipboardContext};
 fn main() {
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     let qlip = ctx.get_contents().unwrap();
-    qr2term::print_qr(qlip);
+    qr2term::print_qr(qlip)
+        .expect("Clipboard contents could not generate usable code.");
 }
